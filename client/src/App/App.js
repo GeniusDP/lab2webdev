b30a01fd001aa85a194393d8562d23f5afe80d7e
GeniusDP <zaranikbz@gmail.com>
2022-01-06 17:52:44 +0200

    finally to modal window

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -85,18 +85,24 @@ function App() {
                     setInfoText(
                         "You cannot write a new letter more the 1 time per 30 sec. Try later."
                     );
-                    setModalForInfoIsOpen(true);
+                    //setModalForInfoIsOpen(true);
                     return;
                 }
                 if (body.sent) {
                     setInfoText("Your letter was sent.");
-                    setModalForInfoIsOpen(true);
+                    //setModalForInfoIsOpen(true);
                     return;
                 }
             })
             .catch((error) => {
-                setInfoText("Something went wrong with server...\n Try later\n" + error);
+                setInfoText(
+                    "Did not sent your mail. Something went wrong with server...\n Try later\n" +
+                        error
+                );
                 setLoaderIsVisible(false);
+                //setModalForInfoIsOpen(true);
+            })
+            .finally(() => {
                 setModalForInfoIsOpen(true);
             });
     }
